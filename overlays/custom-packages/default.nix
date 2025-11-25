a5f256c86ab4a9a88cef0acf053e4535dde01ea5
Markku Ahvenjärvi <mankku@gmail.com>
2025-11-25 15:58:04 +0200

    virtiofsd: add support for pkvm-x86
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- overlays/custom-packages/default.nix
+++ overlays/custom-packages/default.nix
@@ -21,6 +21,7 @@
   osquery-with-hostname = import ./osquery-with-hostname { inherit prev; };
   papirus-icon-theme = import ./papirus-icon-theme { inherit prev; };
   spire = import ./spire { inherit prev; };
+  virtiofsd = import ./virtiofsd { inherit prev; };
   xdg-desktop-portal-cosmic = import ./cosmic/xdg-desktop-portal-cosmic { inherit prev; };
 })
 # keep-sorted end
