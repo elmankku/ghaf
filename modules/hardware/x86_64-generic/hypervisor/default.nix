f989c9be6a456c1c2f967805a50bc8121ef16991
Markku Ahvenjärvi <mankku@gmail.com>
2025-12-04 13:51:52 +0200

    feat(pkvm): add pKVM Intel x86 hypervisor support
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

---
+++ modules/hardware/x86_64-generic/hypervisor/default.nix
@@ -0,0 +1,140 @@
+# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.ghaf.virtualization.pkvm;
+  # Enabled-only VM discovery to avoid touching disabled VMs.
+  enabledSysVms = config.ghaf.virtualization.microvm.sysvm.enabledVms;
+  enabledAppVms = config.ghaf.virtualization.microvm.appvm.enabledVms;
+  allVms = lib.unique ((lib.attrNames enabledSysVms) ++ (lib.attrNames enabledAppVms));
+
+  guestConfig = vmName: cfg.guests.${vmName};
+  mkPkvmEvaluatedConfig =
+    vmName: evaluatedConfig:
+    evaluatedConfig.extendModules {
+      modules = [ (pkvmGuestModule (guestConfig vmName)) ];
+    };
+
+  pkvmGuestModule = vmCfg: { config, ... }: {
+    assertions = [
+      {
+        assertion = config.microvm.hypervisor == "crosvm";
+        message = "pKVM-protected guests must use crosvm.";
+      }
+    ];
+
+    boot.kernelPackages = pkgs.linuxPackagesFor (
+      vmCfg.kernelPackage.override {
+        argsOverride.kernelPatches = config.boot.kernelPatches;
+      }
+    );
+
+    microvm = {
+      crosvm = lib.mkIf (config.microvm.hypervisor == "crosvm") {
+        extraArgs = [
+          "--protected-vm-without-firmware"
+        ];
+        package = lib.mkForce cfg.crosvm.package;
+      };
+    };
+  };
+
+  # The guests are protected VMs by default, but it is possible to opt-out
+  # by setting pkvm.<vm>.enableConfig = false.
+  isConfigEnabled = vmName: cfg.enable && (guestConfig vmName).enableConfig;
+
+  sysVmOverrides = lib.foldl' lib.recursiveUpdate { } (
+    lib.mapAttrsToList (
+      vmType: vmCfg:
+      lib.optionalAttrs (isConfigEnabled vmType && vmCfg.evaluatedConfig != null) {
+        "${vmCfg.vmName}" = {
+          # We must use mkForce to ensure pKVM settings are applied
+          evaluatedConfig = lib.mkForce (mkPkvmEvaluatedConfig vmType vmCfg.evaluatedConfig);
+        };
+      }
+    ) enabledSysVms
+  );
+
+  appVmOverrides = lib.foldl' lib.recursiveUpdate { } (
+    lib.mapAttrsToList (
+      vmType: vmCfg:
+      lib.optionalAttrs (isConfigEnabled vmType && vmCfg.evaluatedConfig != null) {
+        "${vmCfg.name}-vm" = {
+          # We must use mkForce to ensure pKVM settings are applied
+          evaluatedConfig = lib.mkForce (mkPkvmEvaluatedConfig vmType vmCfg.evaluatedConfig);
+        };
+      }
+    ) enabledAppVms
+  );
+in
+{
+  _file = ./default.nix;
+
+  options.ghaf.virtualization.pkvm = {
+    enable = lib.mkOption {
+      description = "Enable pKVM hypervisor";
+      type = lib.types.bool;
+      default = false;
+    };
+
+    hostKernelPackage = lib.mkOption {
+      description = "Kernel package for the host";
+      type = lib.types.package;
+      default = pkgs.linux-pkvm-x86;
+    };
+
+    hostKernelParams = lib.mkOption {
+      description = "Additional kernel parameters for the host";
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "kvm-intel.pkvm=1"
+      ];
+    };
+
+    crosvm.package = lib.mkOption {
+      description = "The crosvm package used for pKVM guests.";
+      type = lib.types.package;
+      default = pkgs.pkvm-crosvm;
+    };
+
+    guests = lib.mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule {
+          options = {
+            enableConfig = lib.mkOption {
+              description = "Enable guest VM configuration";
+              type = lib.types.bool;
+              default = true;
+            };
+
+            kernelPackage = lib.mkOption {
+              description = "Kernel package for the guest VM.";
+              type = lib.types.package;
+              default = pkgs.linux-pkvm-x86-guest;
+            };
+          };
+        }
+      );
+      description = "pKVM settings for individual guest VMs";
+      default = { };
+    };
+  };
+
+  config = lib.mkMerge [
+    {
+      ghaf.virtualization.pkvm.guests = lib.genAttrs allVms (_vmName: { });
+    }
+    (lib.mkIf cfg.enable {
+      boot.kernelPackages = pkgs.linuxPackagesFor cfg.hostKernelPackage;
+      boot.kernelParams = lib.mkAfter cfg.hostKernelParams;
+    })
+    {
+      microvm.vms = lib.recursiveUpdate sysVmOverrides appVmOverrides;
+    }
+  ];
+}
