1a07d47cce51fa979896646e1d2367ead747859c
Markku Ahvenjärvi <mankku@gmail.com>
2025-12-05 15:14:33 +0200

    pkvm-x86: add options host and guest configs
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- modules/hardware/x86_64-generic/hypervisor/default.nix
+++ modules/hardware/x86_64-generic/hypervisor/default.nix
@@ -11,11 +11,17 @@ let
   cfg = config.ghaf.virtualization.pkvm;
   allVms = lib.filterAttrs (_name: vmCfg: vmCfg ? extraModules) options.ghaf.virtualization.microvm;
 
-  pkvmGuestModule = {
-    boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux-pkvm-x86-guest;
+  guestConfig = vmName: cfg.guests.${vmName};
+
+  pkvmGuestModule = vmCfg: {
+    boot.kernelPackages = pkgs.linuxPackagesFor vmCfg.kernelPackage;
 
     microvm = {
-      hypervisor = "qemu";
+      hypervisor = vmCfg.vmm;
+
+      crosvm.extraArgs = [
+        "--protected-vm-without-firmware"
+      ];
 
       qemu = {
         machine = "q35";
@@ -37,9 +43,9 @@ let
     };
   };
 
-  # The guests are protected VMs by default, but it is possible to opt-out of
-  # the default configuration by setting pkvm.<vm>.protectedGuest = false.
-  isProtectedGuest = vmName: cfg.enable && (cfg.guests.${vmName}.protectedGuest or true);
+  # The guests are protected VMs by default, but it is possible to opt-out
+  # by setting pkvm.<vm>.enableConfig = false.
+  isConfigEnabled = vmName: cfg.enable && (guestConfig vmName).enableConfig;
 in
 {
   options.ghaf.virtualization.pkvm = {
@@ -49,14 +55,48 @@ in
       default = false;
     };
 
+    hostKernelPackage = lib.mkOption {
+      description = "Kernel package for the host";
+      type = lib.types.package;
+      default = pkgs.linux-pkvm-x86;
+    };
+
+    hostKernelParams = lib.mkOption {
+      description = "Additional kernel parameters for the host";
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "kvm-intel.pkvm=1"
+        "intel_iommu=sm_on"
+        # FIXME: DEBUGGING
+        "earlyprintk=ttyS0"
+        "ignore_loglevel"
+        "console=ttyS0"
+      ];
+    };
+
     guests = lib.mkOption {
       type = lib.types.attrsOf (
         lib.types.submodule {
           options = {
-            protectedGuest = lib.mkOption {
-              description = "Controls whether this guest is a pKVM protected VM.";
+            enableConfig = lib.mkOption {
+              description = "Enable guest VM configuration";
               type = lib.types.bool;
               default = true;
+            };
+
+            vmm = lib.mkOption {
+              description = "Hypervisor option passed to microvm.";
+              type = lib.types.enum [
+                "qemu"
+                "crosvm"
+              ];
+              default = "qemu";
+            };
+
+            kernelPackage = lib.mkOption {
+              description = "Kernel package for the guest VM.";
+              type = lib.types.package;
+              default = pkgs.linux-pkvm-x86-guest;
             };
           };
         }
@@ -67,23 +107,19 @@ in
   };
 
   config = lib.mkMerge [
+    {
+      ghaf.virtualization.pkvm.guests = lib.genAttrs (lib.attrNames allVms) (_vmName: { });
+    }
     (lib.mkIf cfg.enable {
-      boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux-pkvm-x86;
-      boot.kernelParams = lib.mkAfter [
-        "kvm-intel.pkvm=1"
-        "intel_iommu=sm_on"
-        # FIXME: DEBUGGING
-        "earlyprintk=ttyS0"
-        "ignore_loglevel"
-        "console=ttyS0"
-      ];
+      boot.kernelPackages = pkgs.linuxPackagesFor cfg.hostKernelPackage;
+      boot.kernelParams = lib.mkAfter cfg.hostKernelParams;
     })
     {
       ghaf.virtualization.microvm = lib.mapAttrs (
         vmName: _vmCfg:
-        lib.optionalAttrs (isProtectedGuest vmName) {
+        lib.optionalAttrs (isConfigEnabled vmName) {
           extraModules = lib.mkAfter [
-            pkvmGuestModule
+            (pkvmGuestModule (guestConfig vmName))
           ];
         }
       ) allVms;
