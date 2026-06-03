6636499c4eaa501e6cc283e15f9d769c8b5cb373
Markku Ahvenjärvi <mankku@gmail.com>
2026-06-03 15:31:37 +0300

    pkvm: x86: fix virtiofsd for crosvm
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- packages/pkgs-by-name/crosvm-pkvm/package.nix
+++ packages/pkgs-by-name/crosvm-pkvm/package.nix
@@ -8,6 +8,7 @@ crosvm.overrideAttrs (
   _finalAttrs: oldAttrs: {
     patches = oldAttrs.patches ++ [
       ./0001-crosvm-FIXME-always-trap-MSI-X-region.patch
+      ./0001-vhost-user-handle-ACCESS_PLATFORM-for-protected-gues.patch
     ];
   }
 )
