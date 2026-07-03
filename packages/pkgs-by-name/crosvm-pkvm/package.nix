ba96741b8f7caf5395fdf71baaa120efb16ab303
Markku Ahvenjärvi <mankku@gmail.com>
2026-07-03 16:53:14 +0300

    WIP: crosvm: add patches for Intel GPU passthrough
    TODO: Cleanup patches.
    
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- packages/pkgs-by-name/crosvm-pkvm/package.nix
+++ packages/pkgs-by-name/crosvm-pkvm/package.nix
@@ -9,6 +9,16 @@ crosvm.overrideAttrs (
     patches = oldAttrs.patches ++ [
       ./0001-crosvm-FIXME-always-trap-MSI-X-region.patch
       ./0001-vhost-user-handle-ACCESS_PLATFORM-for-protected-gues.patch
+      ./0001-pci-handle-Intel-GPU-OpRegion-access.patch
+      ./0002-evdev-allow-empty-unique-identifier.patch
+      ./0003-WIP-input-hotplug.patch
+      ./0004-DEBUG-GPU-ACPI-errors.patch
+      ./0005-WIP-Fix-intel-DSM-size.patch
+      ./0006-WIP-More-Intel-GPU-debug.patch
+      ./0007-WIP-shadow-and-log-Intel-GPU-OPRegion-accesses.patch
+      ./0008-HACK-disable-Intel-GPU-AML-generation.patch
+      ./0009-temporarily-disable-logging.patch
+      ./0010-WIP-Do-not-advertise-hotplug-devices-in-ACPI-VIOT.patch
     ];
   }
 )
