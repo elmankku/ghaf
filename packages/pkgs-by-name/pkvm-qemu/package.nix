af48231228c357cb5e272715bfad86a28d3e339c
Markku Ahvenjärvi <mankku@gmail.com>
2026-08-24 14:20:42 +0300

    WIP: fix(pkvm): Fix virtiofsd for QEMU protected VMs
    Signed-off-by: Markku Ahvenjärvi <mankku@gmail.com>

--- packages/pkgs-by-name/pkvm-qemu/package.nix
+++ packages/pkgs-by-name/pkvm-qemu/package.nix
@@ -12,6 +12,7 @@ ghaf-qemu.overrideAttrs (
         url = "https://raw.githubusercontent.com/jkrh/pkvm-x86/bfded00f32f3cba69c4ea7a082fab9ef0898eab8/patches/qemu/0001-qemu-Add-support-for-pKVM.patch";
         hash = "sha256-p1FHTfxeABBW2vFZQexdRY/w8w3kXU+aX87ZEhUb+xQ=";
       })
+      ./0001-virtio-Allow-non-IOTLB-vhost-user-backends-with-conf.patch
     ];
   }
 )
